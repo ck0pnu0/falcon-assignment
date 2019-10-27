72fda353c3cb148baa44dfc7f4b80d35a3828f32
gdimitrov-propy <g.dimitrov@propy.com>
2019-10-27 11:23:17 +0200

    websocket error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
+import { WebsocketService } from './websocket.service';
+// import { WebsocketService } from "./shared/services/websocket.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'falcon-assignment';
+export class AppComponent implements OnInit {
+  title = "falcon-assignment";
+  downstream;
+
+  constructor(private webSocketService: WebsocketService) {}
+
+  ngOnInit() {
+    this.webSocketService.createWebsocketConnection();
+    this.webSocketService.getDownstream().subscribe(data => console.log(data));
+  }
 }
