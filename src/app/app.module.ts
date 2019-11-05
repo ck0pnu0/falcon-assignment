60267ca4393f59e37afb57293cffd4b5f5641870
gdimitrov-propy <g.dimitrov@propy.com>
2019-11-05 13:04:34 +0200

    remove sweetAlert from the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { SweetAlert2Module } from "@sweetalert2/ngx-sweetalert2";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeModule } from "./home/home.module";
@@ -16,8 +15,7 @@ import { HomeModule } from "./home/home.module";
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
       maxAge: 25
-    }),
-    SweetAlert2Module.forRoot()
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
